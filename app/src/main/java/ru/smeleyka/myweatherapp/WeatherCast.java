a25c0acae89fce2b06a83f6a17aa25559d8d9c6b
smeleyka <smeleyka@gmail.com>
2017-08-30 17:47:40 +0300

    3

--- app/src/main/java/ru/smeleyka/myweatherapp/WeatherCast.java
+++ app/src/main/java/ru/smeleyka/myweatherapp/WeatherCast.java
@@ -5,25 +5,11 @@ package ru.smeleyka.myweatherapp;
  */
 
 public class WeatherCast {
-    //private String cities[];
     private String weather[];
 
-    public WeatherCast(String[] cities, String[] weather) {
-        //this.cities = cities;
-        this.weather = weather;
-    }
-
     public WeatherCast(MainActivity obj) {
-       //this.cities = obj.getResources().getStringArray(R.array.city_list);
         this.weather = obj.getResources().getStringArray(R.array.weather_list);
     }
-
-//    public String get_weather (String city){
-//        for (int i = 0; i < cities.length; i++) {
-//            if (city.equals(cities[i])) return weather[i];
-//        }
-//        return null;
-//        }
 
     public String get_weather (int id){
         if (id >= 0 && id < weather.length) return weather[id];
